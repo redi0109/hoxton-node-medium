3f7f29e262b283fa8d09f18bca31a192adfb1173
Demir Boraj <demirboraj97@gmail.com>
2022-09-16 02:02:58 +0200

    1

--- src/server.ts
+++ src/server.ts
@@ -28,4 +28,55 @@ app.get("/users/:id", async (req, res) => {
   }
 });
 
-app.listen(port);
+app.get("/posts", async (req, res) => {
+  const posts = await prisma.post.findMany({
+    include: { user: true, likes: true, comments: { include: { user: true } } },
+  });
+  res.send(posts);
+});
+
+app.get("/posts/:id", async (req, res) => {
+  const id = Number(req.params.id);
+  const post = await prisma.post.findUnique({
+    where: { id: id },
+    include: { user: true, likes: true, comments: { include: { user: true } } },
+  });
+  if (post) {
+    res.send(post);
+  } else {
+    res.status(404).send({ error: "post not found." });
+  }
+});
+
+app.post("/posts", async (req, res) => {
+  await prisma.post.create({ data: req.body });
+  const posts = await prisma.post.findMany({
+    include: { user: true, likes: true, comments: { include: { user: true } } },
+  });
+  res.send(posts);
+});
+
+app.post("/likes", async (req, res) => {
+  const likes = await prisma.like.create({ data: req.body });
+  const post = await prisma.post.findUnique({
+    where: { id: likes.postId },
+    include: { user: true, likes: true, comments: { include: { user: true } } },
+  });
+  res.send(post);
+});
+
+app.post("/comments", async (req, res) => {
+  const comment = await prisma.comment.create({
+    data: req.body,
+    include: { user: true },
+  });
+  const post = await prisma.post.findUnique({
+    where: { id: comment.postId },
+    include: { user: true, likes: true, comments: { include: { user: true } } },
+  });
+  res.send(post);
+});
+
+app.listen(port, () => {
+  console.log(`port: http://localhost:${port}`);
+});
